387fdb20cf874d9794dd011be4f991ab550eab8f
lh <860648679@qq.com>
2024-08-02 19:12:20 +0800

    创建区块链

--- goBlockChain.go
+++ goBlockChain.go
@@ -9,75 +9,191 @@ import (
 	"time"
 )
 
+//使用共识算法计算挖矿时间
+
+// 挖矿难度
+const (
+	MINING_DIFFICULTY = 3
+	MINING_SENDER     = "THE BLOCKCHAIN"
+	MINING_REWARD     = 1.0
+)
+
+// 定义一个区块对象
 type Block struct {
-	nonce        int
-	previousHash string
 	timestamp    int64
-	transactions []string
+	nonce        int
+	previousHash [32]byte
+	transactions []*Transaction
 }
 
-func newBlock(nonce int, previousHash string) *Block {
+func newBlock(nonce int, previousHash [32]byte, transactions []*Transaction) *Block {
 	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
 	b.nonce = nonce
 	b.previousHash = previousHash
-	b.timestamp = time.Now().Unix()
+	b.transactions = transactions
 	return b
 }
 
 func (b *Block) Hash() [32]byte {
 	m, _ := json.Marshal(b)
-	fmt.Println(string(m))
 	return sha256.Sum256([]byte(m))
-	/*b.transactions = append(b.transactions, string(hash[:]))*/
 }
 
+// 重写序列化方法(开头不能是小写)
 func (b *Block) MarshalJSON() ([]byte, error) {
 	return json.Marshal(struct {
-		Nonce        int
-		PreviousHash string
-		Timestamp    int64
-		Transactions []string
+		Timestamp    int64          `json:"timestamp"`
+		Nonce        int            `json:"nonce"`
+		PreviousHash string         `json:"previous_hash"`
+		Transactions []*Transaction `json:"transactions"`
 	}{
-		Nonce:        b.nonce,
-		PreviousHash: b.previousHash,
 		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: fmt.Sprintf("%x", b.previousHash),
 		Transactions: b.transactions,
 	})
 }
 
 func (b *Block) Print() {
-	fmt.Println("nonce:", b.nonce)
-	fmt.Println("previousHash:", b.previousHash)
-	fmt.Println("timestamp:", b.timestamp)
-	fmt.Println("transactions:", b.transactions)
+	fmt.Printf("timestamp:           %d\n", b.timestamp)
+	fmt.Printf("nonce:               %d\n", b.nonce)
+	fmt.Printf("previous_hash:       %x\n", b.previousHash)
+	for _, t := range b.transactions {
+		t.Print()
+	}
 }
 
 type BlockChain struct {
-	transactionPool []string
-	chain           []*Block
+	transactionPool   []*Transaction
+	chain             []*Block
+	blockChainAddress string //区块链节点地址
 }
 
-func NewBlockChain() *BlockChain {
+// 定义一个区块链对象
+func NewBlockChain(blockChainAddress string) *BlockChain {
+	b := &Block{}
 	bc := new(BlockChain)
-	bc.CreateBlock(0, "Init hash")
+	bc.blockChainAddress = blockChainAddress
+	bc.CreateBlock(0, b.Hash())
 	return bc
 }
 
-func (bc *BlockChain) CreateBlock(nonce int, previousHash string) *Block {
-	b := newBlock(nonce, previousHash)
-	m, _ := json.Marshal(b)
-	sum256 := sha256.Sum256([]byte(m))
-	bc.transactionPool = append(bc.transactionPool, string(sum256[:]))
+func (bc *BlockChain) CreateBlock(nonce int, previousHash [32]byte) *Block {
+	b := newBlock(nonce, previousHash, bc.transactionPool)
 	bc.chain = append(bc.chain, b)
+	bc.transactionPool = []*Transaction{}
 	return b
 }
 
+func (bc *BlockChain) LastBlock() *Block {
+	//返回最后一个区块
+	return bc.chain[len(bc.chain)-1]
+}
+
 func (bc *BlockChain) Print() {
-	for i, b := range bc.chain {
-		fmt.Println("%s Chin %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
-		b.Print()
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
 	}
-	fmt.Println("%s\n", strings.Repeat("*", 50))
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
+// 复制事务
+func (bc *BlockChain) CopyTransactionPool() []*Transaction {
+	transactions := make([]*Transaction, 0)
+	for _, t := range bc.transactionPool {
+		transactions = append(transactions,
+			NewTransaction(t.senderBlockchainAddress,
+				t.recipientBlockchainAddress,
+				t.value))
+	}
+	return transactions
+}
+
+// 检验找的哈希是否满足工作量证明的要求
+func (bc *BlockChain) ValidProof(nonce int, previousHash [32]byte, transactions []*Transaction, difficulty int) bool {
+	//比较新区块哈希值的基准(前面是几个0,控制挖矿难度)
+	//0越少,找到有效哈希值所需的计算工作越少，挖矿相对容易
+	zeros := strings.Repeat("0", difficulty)
+	guessBlock := Block{0, nonce, previousHash, transactions}
+	//获得区块的哈希值
+	guessHashStr := fmt.Sprintf("%x", guessBlock.Hash())
+	//fmt.Println(guessHashStr)
+	return guessHashStr[:difficulty] == zeros
+}
+
+func (bc *BlockChain) ProofOfWork() int {
+	//上个区块的事务
+	transactions := bc.CopyTransactionPool()
+	//上个区块的hash
+	previousHash := bc.LastBlock().Hash()
+	nonce := 0
+	for !bc.ValidProof(nonce, previousHash, transactions, MINING_DIFFICULTY) {
+		nonce += 1
+	}
+	return nonce
+}
+
+func (bc *BlockChain) Mining() bool {
+	bc.AddTransaction(MINING_SENDER, bc.blockChainAddress, MINING_REWARD)
+	nonce := bc.ProofOfWork()
+	previousHash := bc.LastBlock().Hash()
+	bc.CreateBlock(nonce, previousHash)
+	log.Println("action=mining, status=success")
+	return true
+}
+
+func (bc *BlockChain) CalculateTotalAmount(blockChainAddress string) float32 {
+	var totalAmount float32 = 0.0
+	for _, c := range bc.chain {
+		transactions := c.transactions
+		for _, t := range transactions {
+			value := t.value
+			if t.recipientBlockchainAddress == blockChainAddress {
+				totalAmount += value
+			}
+			if t.senderBlockchainAddress == blockChainAddress {
+				totalAmount -= value
+			}
+		}
+	}
+	return totalAmount
+}
+
+// 定义一个事务对象
+type Transaction struct {
+	senderBlockchainAddress    string
+	recipientBlockchainAddress string
+	value                      float32
+}
+
+func NewTransaction(sender string, recipient string, value float32) *Transaction {
+	return &Transaction{sender, recipient, value}
+}
+
+func (t *Transaction) Print() {
+	fmt.Printf("%s\n", strings.Repeat("-", 50))
+	fmt.Printf("sender_blockchain_address: %s\n", t.senderBlockchainAddress)
+	fmt.Printf("recipient_blockchain_address: %s\n", t.recipientBlockchainAddress)
+	fmt.Printf("value: %.1f\n", t.value)
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Sender    string  `json:"sender_blockchain_address"`
+		Recipient string  `json:"recipient_blockchain_address"`
+		Value     float32 `json:"value"`
+	}{
+		Sender:    t.senderBlockchainAddress,
+		Recipient: t.recipientBlockchainAddress,
+		Value:     t.value,
+	})
+}
+
+func (bc *BlockChain) AddTransaction(sender string, recipient string, value float32) {
+	transaction := NewTransaction(sender, recipient, value)
+	bc.transactionPool = append(bc.transactionPool, transaction)
 }
 
 func init() {
@@ -85,12 +201,29 @@ func init() {
 }
 
 func main() {
-	/*blockChain := NewBlockChain()
+	//初始化区块链
+	myBlockChainAddress := "my_blockChain_address"
+	blockChain := NewBlockChain(myBlockChainAddress)
+	//打印
 	blockChain.Print()
-	blockChain.CreateBlock(5, "hash 1")
+
+	//为初始的区块添加一条事务
+	blockChain.AddTransaction("A", "B", 1.0)
+	blockChain.Mining()
 	blockChain.Print()
-	blockChain.CreateBlock(2, "hash 2")
-	blockChain.Print()*/
-	block := newBlock(1, "232")
-	fmt.Printf("%x\n", block.Hash())
+
+	blockChain.AddTransaction("C", "D", 2.0)
+	blockChain.AddTransaction("E", "F", 3.0)
+	blockChain.Mining()
+	blockChain.Print()
+
+	fmt.Printf("my	 %.1f\n", blockChain.CalculateTotalAmount("my_blockChain_address"))
+	fmt.Printf("C %.1f\n", blockChain.CalculateTotalAmount("C"))
+	fmt.Printf("D %.1f\n", blockChain.CalculateTotalAmount("D"))
 }
+
+/**
+---------------------------------总结---------------------------------------
+区块：每一个区块除了存储各种交易信息外，还存储上一个区块的hash,
+区块链：将这些区块连接起来的链(定义两个数组,分别存每个区块的hash和每个区块的对象,hash的下标对应相应的区块)
+*/
